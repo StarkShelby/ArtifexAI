56735fda35f0256efc1085b566e9ec55cfd2df3f
Sharique Rahmani <sharique@Shariques-MacBook-Air.local>
2025-06-15 20:24:15 +0530

    Update files frontend link

--- server/index.js
+++ server/index.js
@@ -9,12 +9,12 @@ import openai from './Routes/openai.js'
 dotenv.config()
 
 const app = express();
+
+
 app.use(cors({
   origin: 'https://artifex-ai-fawn.vercel.app',  // your frontend domain
   credentials: true
 }))
-
-app.use(cors())
 app.use(express.json({limit: '50mb'}))
 
 app.use('/api/v1/post', postRoutes)
